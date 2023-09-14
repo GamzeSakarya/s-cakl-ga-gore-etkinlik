58657303c76665e172bc10c67365d331b5b49637
Gamze Sakarya <126356427+GamzeSakarya@users.noreply.github.com>
2023-09-14 13:43:57 +0300

    Add files via upload

---
+++ sıcaklığa-göre-etkinlik/src/com/company/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık Giriniz : ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak Yapabilirsiniz.");
+        } else if (heat <= 10) {
+            System.out.println("Pikniğe Gidebilirsiniz.");
+        } else if (heat <= 15) {
+            System.out.println("Sinemaya Gidebilirsiniz.");
+        } else if (heat <= 25) {
+            System.out.println("Dışarıda Yürüyüş Yapabilirsiniz.");
+        } else {
+            System.out.println("Yüzmeye Gidebilirsiniz.");
+        }
+
+    }
+
+}
